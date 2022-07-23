93442ae8b9a157e87d8838be56e6309fc4025ad6
Ngi Bujri <nbujri7@gmail.com>
2022-07-23 15:52:43 -0400

    Add buttons and display to HTML

--- calculator.js
+++ calculator.js
@@ -13,3 +13,24 @@ function multiply(...args) {
 function divide(...args) {
     return args.reduce((prev, current) => prev / current);
 }
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+            break;
+        case '-':
+            return subtract(num1, num2);
+            break;
+        case '*':
+            return multiply(num1, num2);
+            break;
+        case '/':
+            return divide(num1, num2);
+            break;
+        default:
+            return 'ERROR!'
+            break;
+    }
+}
+console.log(operate('+',5, 5))
