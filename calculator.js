76c4c7b66729e2e35212941c3e6b7f452fccc82a
Ngi Bujri <nbujri7@gmail.com>
2022-07-24 20:31:58 -0400

    Add ability to find solution to two numbers and display it using buttons

--- calculator.js
+++ calculator.js
@@ -14,7 +14,7 @@ function divide(...args) {
     return args.reduce((prev, current) => prev / current);
 }
 
-function operate(operator, num1, num2) {
+function operate(operator, num1, num2) { // take operator and 2 nums and return solution
     switch (operator) {
         case '+':
             return add(num1, num2);
@@ -34,14 +34,46 @@ function operate(operator, num1, num2) {
     }
 }
 
-function displayNum() {
+function displayNum() { // display value of clicked buttons
     displayValue += this.textContent;
     display.value = displayValue;
 }
 
+function clear() {
+    displayValue = '';
+    display.value = displayValue;
+    currentOperator = '';
+}
+
+function getOperator() {
+    currentOperator = this.textContent;
+    num1 = parseFloat(displayValue);
+    displayValue = '';
+}
+
+function displaySolution() {
+    num2 = parseFloat(displayValue);
+    console.log(`${num1} ${currentOperator} ${num2}`)
+    if (!currentOperator || !num1 || !num2) {
+        display.value = 'ERROR!';
+        setTimeout(clear, 2000);
+    } else {
+        display.value = operate(currentOperator, num1, num2);
+    }
+}
+
 let displayValue = '';
+let currentOperator = '';
+let num1, num2;
 const numButtons = document.querySelectorAll('.num-btn');
 const display = document.querySelector('.display');
+const clearButton = document.querySelector('.clear-btn');
+const opButtons = document.querySelectorAll('.op-btn');
+const equalButton = document.querySelector('.equal-btn');
 
-numButtons.forEach((btn) => btn.addEventListener('click', displayNum))
-display.disabled = true
+display.disabled = true;
+numButtons.forEach((btn) => btn.addEventListener('click', displayNum));
+opButtons.forEach((btn) => btn.addEventListener('click', getOperator))
+clearButton.addEventListener('click', clear);
+equalButton.addEventListener('click', displaySolution);
+
